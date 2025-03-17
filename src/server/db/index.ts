be46fbcbcfd45491523206ff1721789b1536ea88
sinskiy <dimasinskiy@gmail.com>
2025-03-17 20:05:30 +0300

    chore: set up drizzle, better-auth and turso

---
+++ src/server/db/index.ts
@@ -0,0 +1,11 @@
+import "@/lib/env";
+import { drizzle } from "drizzle-orm/libsql";
+
+// You can specify any property from the libsql connection options
+const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
+export default db;
