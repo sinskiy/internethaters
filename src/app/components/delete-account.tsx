cac6b4654c71fcc92d9b76056acb12d5eb7c08c2
sinskiy <dimasinskiy@gmail.com>
2025-03-21 16:58:14 +0300

    feat: type message to delete account

--- src/app/components/delete-account.tsx
+++ src/app/components/delete-account.tsx
@@ -2,9 +2,10 @@
 
 import components from "@/app/components.module.css";
 import { deleteAccountAction } from "@/server/actions";
-import { RefObject, useRef } from "react";
+import { RefObject, useRef, useState } from "react";
 import Form from "../ui/form";
 import { DialogCloseButton } from "../ui/dialog-close-button";
+import InputField from "../ui/input-field";
 
 interface Props {
   username: string;
@@ -14,6 +15,11 @@ export default function DeleteAccount({ username }: Props) {
   const dialogRef = useRef<HTMLDialogElement>(
     null
   ) as RefObject<HTMLDialogElement>;
+
+  const message = `i, ${username}, want to delete my account`;
+
+  const [deletionConfirmation, setDeletionConfirmation] = useState("");
+
   return (
     <>
       <button
@@ -30,7 +36,17 @@ export default function DeleteAccount({ username }: Props) {
           action={() => deleteAccountAction(username)}
           buttonsStyle="dangerous"
           secondButton={<DialogCloseButton dialogRef={dialogRef} />}
-        ></Form>
+          buttonProps={{ disabled: deletionConfirmation !== message }}
+        >
+          <InputField
+            id="deletion-confirmation"
+            label={`type "${message}"`}
+            type="text"
+            value={deletionConfirmation}
+            onChange={(e) => setDeletionConfirmation(e.currentTarget.value)}
+            required
+          />
+        </Form>
       </dialog>
     </>
   );
