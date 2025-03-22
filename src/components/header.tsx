f4f53f8c8c0a731b8d7185f2f43f7da63eadd29b
sinskiy <dimasinskiy@gmail.com>
2025-03-22 16:01:01 +0300

    feat: vc link icon top nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,13 +4,23 @@ import Home from "@/icons/home";
 import { Suspense } from "react";
 import { UserOrAuth } from "./user";
 import components from "@/app/components.module.css";
+import Headphones from "@/icons/headphones";
 
 export default function Header() {
   return (
     <header className={classes.header}>
-      <Link href="/" aria-label="home" className={components.activatable}>
-        <Home />
-      </Link>
+      <nav className={classes.nav}>
+        <Link href="/" aria-label="home" className={components.activatable}>
+          <Home />
+        </Link>
+        <Link
+          href="/voice"
+          aria-label="voice chats"
+          className={components.activatable}
+        >
+          <Headphones />
+        </Link>
+      </nav>
       <Suspense fallback="loading...">
         <UserOrAuth />
       </Suspense>
